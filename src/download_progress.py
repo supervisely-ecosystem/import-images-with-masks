3d34aee1d3f57e2600388695c9f782fc5e69143a
cxnt <pavelbartsits@gmail.com>
2023-02-21 13:43:51 +0400

    add download progress

---
+++ src/download_progress.py
@@ -0,0 +1,16 @@
+import supervisely as sly
+from functools import partial
+
+
+def update_progress(count, api: sly.Api, task_id, progress: sly.Progress):
+    count = min(count, progress.total - progress.current)
+    progress.iters_done(count)
+    if progress.need_report():
+        progress.report_progress()
+
+
+def get_progress_cb(api, task_id, message, total, is_size=False, func=update_progress):
+    progress = sly.Progress(message, total, is_size=is_size)
+    progress_cb = partial(func, api=api, task_id=task_id, progress=progress)
+    progress_cb(0)
+    return progress_cb
