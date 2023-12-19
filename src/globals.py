7f4ab2c1b384179fa0f5b17752265347ad6c1719
almaz <almazgimaev@gmail.com>
2023-12-19 23:04:35 +0100

    check if any data is uploaded

--- src/globals.py
+++ src/globals.py
@@ -23,6 +23,8 @@ if os.environ.get("modal.state.slyProjectId") is not None:
 if os.environ.get("modal.state.slyDatasetId") is not None:
     DATASET_ID = int(os.environ.get("modal.state.slyDatasetId"))
 
+INPUT_FILE = sly.env.file(raise_not_found=False)
+INPUT_DIR = sly.env.folder(raise_not_found=False)
 INPUT_PATH = os.environ.get("modal.state.files", "")
 if INPUT_PATH != "":
     # set env variable for future usage
@@ -30,6 +32,8 @@ if INPUT_PATH != "":
         os.environ["modal.state.slyFolder"] = INPUT_PATH
     else:
         os.environ["modal.state.slyFile"] = INPUT_PATH
+elif INPUT_FILE is None and INPUT_DIR is None:
+    raise RuntimeError("Please specify input file or directory")
 
 IMAGE_DIR_NAME = "img"
 ANNOTATION_DIR_NAME = "ann"
